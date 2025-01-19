c3d892a17125dbfb6ead927dfd7a8fe5670e76ea
Vikas Verma <109075366+Vikas100702@users.noreply.github.com>
2025-01-19 23:28:56 +0530

    Commiting bottom navigation and image slider changes.

--- lib/ui/components/bottom_navigation_bar/bottom_navigation_bar.dart
+++ lib/ui/components/bottom_navigation_bar/bottom_navigation_bar.dart
@@ -1,70 +1,102 @@
+/*
 import 'package:flutter/material.dart';
-import '../../../utils/app_colors.dart';
-import '../../../utils/size_config.dart';
 
-class CustomNavigationBar extends StatelessWidget {
+class CustomNavigationBar extends StatefulWidget {
   const CustomNavigationBar({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    SizeConfig.init(context);
+  State<CustomNavigationBar> createState() => _CustomNavigationBarState();
+}
 
+class _CustomNavigationBarState extends State<CustomNavigationBar> {
+  @override
+  Widget build(BuildContext context) {
     return BottomAppBar(
-      padding: EdgeInsets.zero,
-      height: SizeConfig.height(8),
-      color: Colors.black,
+      color: whiteColor,
+      height: SizeConfig.height(7),
       shape: const CircularNotchedRectangle(),
       notchMargin: SizeConfig.devicePixelRatio(8),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Expanded(
-            child: IconButton(
-              icon: Icon(
-                Icons.calendar_today,
-                color: AppColors.surfaceLight,
-                size: SizeConfig.devicePixelRatio(28),
-              ),
-              onPressed: () {},
-            ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.event),
+            color: blackColor,
           ),
-          Expanded(
-            child: IconButton(
-              icon: Icon(
-                Icons.cloud_outlined,
-                color: AppColors.surfaceLight,
-                size: SizeConfig.devicePixelRatio(28),
-              ),
-              onPressed: () {},
-            ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.cloud),
+            color: blackColor,
           ),
-          Expanded(
-            child: SizedBox(width: SizeConfig.width(8)),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.note_alt),
+            color: blackColor,
           ),
-          Expanded(
-            child: IconButton(
-              icon: Icon(
-                Icons.edit_outlined,
-                color: AppColors.surfaceLight,
-                size: SizeConfig.devicePixelRatio(28),
-              ),
-              onPressed: () {},
-            ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.image),
+            color: blackColor,
           ),
-          Expanded(
-            child: IconButton(
-              icon: Icon(
-                Icons.photo_library_outlined,
-                color: AppColors.surfaceLight,
-                size: SizeConfig.devicePixelRatio(28),
-              ),
-              onPressed: () {},
-            ),
+        ],
+      ),
+    );
+  }
+}*/
+
+import 'package:flutter/material.dart';
+import '../../../utils/app_colors.dart';
+import '../../../utils/size_config.dart'; // Import the colors file
+
+class CustomNavigationBar extends StatefulWidget {
+  const CustomNavigationBar({super.key});
+
+  @override
+  State<CustomNavigationBar> createState() => _CustomNavigationBarState();
+}
+
+class _CustomNavigationBarState extends State<CustomNavigationBar> {
+  @override
+  Widget build(BuildContext context) {
+    final brightness = Theme.of(context).brightness;
+    final backgroundColor = brightness == Brightness.light
+        ? AppColors.surfaceLight
+        : AppColors.surfaceDark;
+    final iconColor = brightness == Brightness.light
+        ? AppColors.textPrimaryLight
+        : AppColors.textPrimaryDark;
+
+    return BottomAppBar(
+      color: backgroundColor,
+      shape: const CircularNotchedRectangle(),
+      notchMargin: SizeConfig.devicePixelRatio(8),
+      padding: EdgeInsets.symmetric(vertical: SizeConfig.height(1)), // Adjust the vertical padding
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.event),
+            color: iconColor,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.cloud),
+            color: iconColor,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.note_alt),
+            color: iconColor,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.image),
+            color: iconColor,
           ),
         ],
       ),
     );
   }
 }
-
-// In your HomeScreen.dart, update the FloatingActionButton:
