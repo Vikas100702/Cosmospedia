2976579842acee2b60ea8f0a9ae8405af92b7a5d
Vikas Verma <109075366+Vikas100702@users.noreply.github.com>
2025-04-28 14:11:39 +0530

    Implementing Splash Screen.

--- lib/app.dart
+++ lib/app.dart
@@ -1,21 +1,17 @@
 import 'package:cosmospedia/config/theme.dart';
 import 'package:cosmospedia/l10n/app_localizations_delegates.dart';
-import 'package:cosmospedia/ui/screens/home_screen/home_screen.dart';
-import 'package:cosmospedia/ui/screens/sign_in_screen/sign_in_screen.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:cosmospedia/ui/screens/splash_screen/splash_screen.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter/material.dart';
 
 import 'config/routes.dart';
-import 'l10n/app_localizations.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
+    /*return StreamBuilder<User?>(
       stream: FirebaseAuth.instance.authStateChanges(),
       builder: (context, snapshot) {
         // User is signed in
@@ -38,46 +34,47 @@ class MyApp extends StatelessWidget {
               debugShowCheckedModeBanner: false,
               initialRoute: '/',
               onGenerateRoute: AppRouter.onGenerateRoute,
-              home: const HomeScreen(),
-              /*home: Builder(
+              home: const SplashScreen(),
+              */
+    /*home: Builder(
                 builder: (context) {
                   // Use a fallback method to avoid null check
                   final l10n = AppLocalizations.of(context) ?? AppLocalizations.of(context);
                   return const SignInScreen();
                 },
               )*/
+    /*
           );
-        }
+        }*/
 
-        // User is not signed in
-        return MaterialApp(
-            localizationsDelegates: const [
-              AppLocalizationsDelegate(),
-              GlobalMaterialLocalizations.delegate,
-              GlobalWidgetsLocalizations.delegate,
-              GlobalCupertinoLocalizations.delegate,
-            ],
-            supportedLocales: const [
-              Locale('en', ''), //English
-              // Add more locales as needed
-            ],
-            title: 'Cosmospedia', // Use a hardcoded fallback title
-            theme: AppTheme.lightTheme,
-            darkTheme: AppTheme.darkTheme,
-            themeMode: ThemeMode.system,
-            debugShowCheckedModeBanner: false,
-            initialRoute: '/',
-            onGenerateRoute: AppRouter.onGenerateRoute,
-            home: const SignInScreen(),
-            /*home: Builder(
+    // User is not signed in
+    return MaterialApp(
+      localizationsDelegates: const [
+        AppLocalizationsDelegate(),
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en', ''), //English
+        // Add more locales as needed
+      ],
+      title: 'Cosmospedia',
+      // Use a hardcoded fallback title
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.system,
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      onGenerateRoute: AppRouter.onGenerateRoute,
+      home: const SplashScreen(),
+      /*home: Builder(
               builder: (context) {
                 // Use a fallback method to avoid null check
                 final l10n = AppLocalizations.of(context) ?? AppLocalizations.of(context);
                 return const SignInScreen();
               },
             )*/
-        );
-      }
     );
   }
 }
